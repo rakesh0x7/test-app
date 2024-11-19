07ba6c8ee56eddb5459d728b436441c6486edad4
rakesh0x7 <19131a05p3@gvpce.ac.in>
2024-11-19 18:46:15 +0530

    vercel changes

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
-const sentryRoutes = require("../testapp/routes/sentry");
+const sentryRoutes = require("./routes/sentry");
 
 // Routes
 app.use('/api/auth', require('./routes/auth'));
